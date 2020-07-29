3b2e3d77287673ff74cd5b9370aab39d79c77d47
Biliane Moreira <bilianemoreira@gmail.com>
2020-07-29 23:12:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//calculadora do valor do projeto - freelancer
+
+var valorHora = document.querySelector("#hora");
+var horasProjeto = document.querySelector("#projeto");
+var valorTotal = document.querySelector("#resposta");
+
+function calcular() {
+
+    var valorTotalProjeto = (valorHora.valueAsNumber * horasProjeto.valueAsNumber).toFixed(2);
+    valorTotal.textContent = "Valor Total do Projeto: R$ " + valorTotalProjeto;
+}
